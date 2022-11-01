5392ef39065cfe2443323d1f807dae52773980f0
Tuấn Hoàng <17521217@gm.uit.edu.vn>
2022-11-01 21:17:33 +0700

    update foods page

--- src/routes/Routers.js
+++ src/routes/Routers.js
@@ -16,8 +16,8 @@ const Routers = () => {
   return <Routes>
     <Route path='/' element={<Navigate to = '/home'/>} />
     <Route path='/home' element={<Home/>} />
-    <Route path='/foods' element={<AllFoods/>} />
     <Route path='/foods/:id' element={<FoodDetails/>} />
+    <Route path='/foods' element={<AllFoods/>} />
     <Route path='/cart' element={<Cart/>} />
     <Route path='/checkout' element={<Checkout/>} />
     <Route path='/login' element={<Login/>} />
