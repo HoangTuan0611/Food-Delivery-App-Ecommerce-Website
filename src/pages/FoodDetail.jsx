a8b8fde2c7716721da73d044ba3f00648e2f4c80
Tuấn Hoàng <17521217@gm.uit.edu.vn>
2022-11-02 10:42:23 +0700

    done food details

--- src/pages/FoodDetail.jsx
+++ src/pages/FoodDetail.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import products from "../assets/fake-data/products";
 import { useParams } from "react-router-dom";
@@ -6,105 +6,177 @@ import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/common-section/CommonSection";
 import { Container, Row, Col } from "reactstrap";
 
-import productImg from "../assets/images/product_01.1.jpg";
 import "../styles/product-details.css";
+import ProductCard from "../components/UI/product-card/ProductCard";
+import { useDispatch } from "react-redux";
+import { cartActions } from "../store/shopping-cart/cartSlice";
 
 const FoodDetail = () => {
+  const [tab, setTab] = useState("desc");
+  const [enteredName, setEnterName] = useState("");
+  const [enteredEmail, setEnterEmail] = useState("");
+  const [reviewMsg, setReviewMsg] = useState("");
+  const { id } = useParams();
+  const dispatch = useDispatch();
+
+  const product = products.find((product) => product.id === id);
+  const { title, price, category, desc, image01 } = product;
+  const [previewImg, setPreviewImg] = useState(product.image01);
+  const relatedProduct = products.filter((item) => category === item.category);
+
+  const addItem = () => {
+    dispatch(
+      cartActions.addItem({
+        id,
+        title,
+        price,
+        image01,
+      })
+    );
+  };
+
+  const submitHandle = e=>{
+    e.preventDefault()
+  }
+
+  useEffect(() => {
+    setPreviewImg(product.image01);
+  }, []);
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [product]);
+
   return (
     <Helmet title="Product-details">
-      <CommonSection title="product 01" />
+      <CommonSection title={title} />
 
       <section>
         <Container>
           <Row>
             <Col md="2">
               <div className="product__images">
-                <div className="img__item">
-                  <img src={productImg} alt="" className="w-50" />
+                <div
+                  className="img__item mb-2"
+                  onClick={() => setPreviewImg(product.image01)}
+                >
+                  <img src={product.image01} alt="" className="w-50" />
                 </div>
-                <div className="img__item">
-                  <img src={productImg} alt="" className="w-50" />
+                <div
+                  className="img__item mb-2"
+                  onClick={() => setPreviewImg(product.image02)}
+                >
+                  <img src={product.image02} alt="" className="w-50" />
                 </div>
-                <div className="img__item">
-                  <img src={productImg} alt="" className="w-50" />
+                <div
+                  className="img__item mb-2"
+                  onClick={() => setPreviewImg(product.image03)}
+                >
+                  <img src={product.image03} alt="" className="w-50" />
                 </div>
               </div>
             </Col>
             <Col md="4">
               <div className="product__main-img">
-                <img src={productImg} alt="" className="w-100" />
+                <img src={previewImg} alt="" className="w-100" />
               </div>
             </Col>
             <Col md="6">
               <div className="single__product-content">
-                <h2 className="product__title mb-3">Pizza with mushroom</h2>
+                <h2 className="product__title mb-3">{title}</h2>
                 <span className="product__price">
-                  Price: <span>$34</span>
+                  Price: <span>${price}</span>
                 </span>
                 <p className="category mb-5">
-                  Category: <span>Burger</span>
+                  Category: <span>{category}</span>
                 </p>
 
-                <button className="addToCart__btn">Add to Cart</button>
+                <button onClick={addItem} className="addToCart__btn">
+                  Add to Cart
+                </button>
               </div>
             </Col>
-
             <Col lg="12">
               <div className="tabs d-flex align-items-center gap-3 py-2">
-                <h6 className="tab__active">Desription</h6>
-                <h6>Review</h6>
+                <h6
+                  className={`${tab === "desc" ? "tab__active" : ""}`}
+                  onClick={() => setTab("desc")}
+                >
+                  Desription
+                </h6>
+                <h6
+                  className={`${tab === "rev" ? "tab__active" : ""}`}
+                  onClick={() => setTab("rev")}
+                >
+                  Review
+                </h6>
               </div>
 
-              <div className="tab__content">
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Quaerat, ipsam. Alias voluptate minima quaerat tempora ut
-                  laudantium amet suscipit natus possimus deserunt iste,
-                  praesentium, reprehenderit pariatur. Esse, est dolorem tempora
-                  illo voluptate sed reprehenderit aut sit sunt, ipsum quia
-                  atque?
-                </p>
-              </div>
-
-              <div className="tab__form">
-                <div className="review">
-                  <p className="user__name mb-0">Jhon Doe</p>
-                  <p className="user__email">john1@gmail.com</p>
-                  <p className="feedback__text">great product</p>
+              {tab === "desc" ? (
+                <div className="tab__content">
+                  <p>{desc}</p>
                 </div>
+              ) : (
+                <div className="tab__form mb-3">
+                  <div className="review pt-5">
+                    <p className="user__name mb-0">Jhon Doe</p>
+                    <p className="user__email">john1@gmail.com</p>
+                    <p className="feedback__text">great product</p>
+                  </div>
 
-                <div className="review">
-                  <p className="user__name mb-0">Jhon Doe</p>
-                  <p className="user__email">john1@gmail.com</p>
-                  <p className="feedback__text">great product</p>
-                </div>
+                  <div className="review">
+                    <p className="user__name mb-0">Jhon Doe</p>
+                    <p className="user__email">john1@gmail.com</p>
+                    <p className="feedback__text">great product</p>
+                  </div>
 
-                <div className="review">
-                  <p className="user__name mb-0">Jhon Doe</p>
-                  <p className="user__email">john1@gmail.com</p>
-                  <p className="feedback__text">great product</p>
+                  <div className="review">
+                    <p className="user__name mb-0">Jhon Doe</p>
+                    <p className="user__email">john1@gmail.com</p>
+                    <p className="feedback__text">great product</p>
+                  </div>
+
+                  <form className="form" onSubmit={submitHandle}>
+                    <div className="form__group">
+                      <input
+                        type="text"
+                        placeholder="Enter your name"
+                        onChange={(e) => setEnterName(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="form__group">
+                      <input
+                        type="text"
+                        placeholder="Enter your email"
+                        onChange={(e) => setEnterEmail(e.target.value)}
+                        required
+                      />
+                    </div>
+                    <div className="form__group">
+                      <textarea
+                        rows={5}
+                        type="text"
+                        placeholder="Write your review"
+                        onChange={(e) => setReviewMsg(e.target.value)}
+                        required
+                      ></textarea>
+                    </div>
+                    <button type="submit" className="addToCart__btn">
+                      Submit
+                    </button>
+                  </form>
                 </div>
-                
-                <form className="form">
-                  <div className="form__group">
-                    <input type="text" placeholder="Enter your name" />
-                  </div>
-                  <div className="form__group">
-                    <input type="text" placeholder="Enter your name" />
-                  </div>
-                  <div className="form__group">
-                    <textarea
-                      rows={5}
-                      type="text"
-                      placeholder="Enter your name"
-                    ></textarea>
-                  </div>
-                  <button type="submit" className="addtoCart__btn">
-                    Submit
-                  </button>
-                </form>
-              </div>
+              )}
             </Col>
+            <Col lg="12" className="mb-5 mt-4">
+              <h2 className="related__Product-title">You might also like</h2>
+            </Col>
+            {relatedProduct.map((item) => (
+              <Col lg="3" md="4" xs="6" className="mb-4" key={item.id}>
+                <ProductCard item={item} />
+              </Col>
+            ))}
           </Row>
         </Container>
       </section>
